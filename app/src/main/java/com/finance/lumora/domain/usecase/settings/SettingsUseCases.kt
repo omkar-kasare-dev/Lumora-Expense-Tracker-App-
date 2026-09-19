ed026e9050d9f846047a8b58aa88897e594087cb
kasare omkar <kasareomkar884@gmail.com>
2026-09-19 17:32:03 +0530

    Modified LumoraDatabase configuration

--- app/src/main/java/com/finance/lumora/domain/usecase/settings/SettingsUseCases.kt
+++ app/src/main/java/com/finance/lumora/domain/usecase/settings/SettingsUseCases.kt
@@ -1,21 +1,5 @@
 package com.finance.lumora.domain.usecase.settings
-/*
-data class SettingsUseCases(
 
-    val getBudget: GetBudgetUseCase,
-
-    val saveBudget: SaveBudgetUseCase,
-
-    val getTheme: GetThemeUseCase,
-
-    val saveTheme: SaveThemeUseCase,
-
-    val getCurrency: GetCurrencyUseCase,
-
-    val saveCurrency: SaveCurrencyUseCase
-)
-
- */
 
 
 data class SettingsUseCases(
